96f003349c5eb4abfed84823ebb9808739fabe2a
MernDevelopers1 <merndeveloper2@gmail.com>
2023-06-16 21:58:56 +0500

    Changes in Schema

--- src/db/Schema.js
+++ src/db/Schema.js
@@ -1,29 +1,137 @@
 const mongoose = require("mongoose");
 const client = mongoose.Schema({
-    Name:{
+    UserID:{
         type: String,
         required: true,
     },
-    Email:{
+    BusinessName:{
         type: String,
         required: true,
     },
-    Phone_Number:{
+    BusinessEmail:{
         type: String,
         required: true,
+    },
+    BusinessPhone:{
+        type: String,
+        required: true,
+    },
+    BusinessAddress:{
+        type: String,
+        required: true,
+    },
+    City:{
+        type: String,
+        required: true,
+    },
+    State:{
+        type: String,
+        required: true,
+    },
+    Zip:{
+        type: String,
+        required: true,
+    },
+    Country:{
+        type: String,
+        required: true,
+    },
+    PrimaryContactName:{
+        type: String,
+        required: true,
+    },
+    PrimaryContactEmail:{
+        type: String,
+        required: true,
+    },
+    PrimaryContactPhone:{
+        type: String,
+        required: true,
+    },
+    LogoUrl:{
+        type: String,
+        required: true,
+    },
+    CoverPhotoUrl:{
+        type: String,
+        required: true,
+    },
+    IsActive:{
+        type: Boolean,
+        required: true,
+        default:true,
     },
     
+    
+    
+},{
+    timestamps:true
 });
-const stores = mongoose.Schema({
-    Client_id:{
+const users = mongoose.Schema({
+    Username:{
+        type:String,
+        required:true
+    },
+    HashedPassword:{
+        type:String,
+        required:true
+    },
+    LastLoginFromIp:{
+        type:String,
+        required:true
+    },
+    LastLoginAt:{
+        type:String,
+        required:true
+    }
+},{
+    timestamps:true
+});
+const role = mongoose.Schema({
+    RoleId:{
+        type:Number,
+        required:true
+    },
+    RoleName:{
+        type:String,
+        required:true
+    }
+},{timestamps:true});
+const userInRole = mongoose.Schema({
+    UserId:{
+        type:String,
+        required:true
+    },
+    RoleId:{
+        type:String,
+        required:true
+    }
+
+},{
+    timestamps:true
+});
+const clientShops = mongoose.Schema({
+    ShopId:{
+        type:String,
+        required:true
+    },
+    ClientId:{
         type:String,
         required:true,
     },
-    Store_Name:{
+    StoreName:{
         type:String,
         required:true,
     },
-    Website:{
+    PrimaryContactName:{
+        type:String,
+        required:true,
+    },
+    PrimaryContactPhone:{
+        type:String,
+        required:true,
+    },
+    PrimayContactEmail:{
         type:String,
         required:true,
     },
@@ -35,128 +143,264 @@ const stores = mongoose.Schema({
         type:String,
         required:true,
     },
+   State:{
+        type:String,
+        required:true,
+    },
+    Zip:{
+        type:String,
+        required:true,
+    },
     Country:{
-        type:String,
-        required:true,
-    },
-    Postal_Code:{
-        type:String,
-        required:true,
-    },
-   Geo_Location:{
-        type:String,
-        required:true,
-    },
-    Logo:{
-        type:String,
-        required:true,
-    },
-    Shop_Images:{
         type:Array,
         required:true,
     },
     
-})
-const customer = mongoose.Schema({
-    Client_id:{
+},{
+    timestamps:true
+});
+const customers = mongoose.Schema({
+    UserId:{
         type:String,
         required:true,
     },
-    Store_id:{
+    FullName:{
         type:String,
     },
-    Customer_Name:{
-        type:String,
+    Phone1:{
+        type:String, 
         required:true,
     },
-    Phone:{
-        type:String,
-        required:true,
-    },
-    Address:{
+    Phone2:{
         type:String,
         required:true,
     },
     Email:{
         type:String,
     },
-    Customer_Images:{
-        type:Array,
-        required:true,
-    },
-    
-})
-const order = mongoose.Schema({
-    Client_id:{
+    Address:{
         type:String,
         required:true,
     },
-    
-    Order_Price:{
-        type:Number,
-        required:true,
-    },
-    Cloth_Id:{
+    City:{
         type:String,
         required:true,
     },
-    Delivery_Date:{
-        type:Date,
+    State:{
+        type:String,
         required:true,
     },
-    Trial_Date:{
-        type:Date,
+    Zip:{
+        type:String,
         required:true,
     },
-    Advance_Payment:{
-        type:Array,
+    Country:{
+        type:String,
         required:true,
     },
+    IsActive:{
+        type:Boolean,
+        required:true
+    }
+
     
+},{
+    timestamps:true
 });
-const cloth = mongoose.Schema({
-    
-    
-    
-    Cloth_Type:{
+const productType = mongoose.Schema({
+    Title:{
+        type:String,
+        required:true
+    },
+    ImageUrl:{
         type:String,
         required:true,
     },
-    Measurment:{
+    MeasurmentAttribute:{
+        type:Object,
+        required:true
+    }
+
+});
+const order = mongoose.Schema({
+    OrderId:{
+        type:String,
+        required:true,
+    },
+    ClientId:{
+        type:String,
+        required:true,
+    },
+    CustomerId:{
+        type:String,
+        required:true,
+    },
+    OrderPlacedDate:{
+        type:Date,
+        required:true,
+    },
+    Status:{
+        type:Date,
+        required:true,
+    },
+    OrderType:{
+        type:Array,
+        required:true,
+    },
+    BillingFullName:{
+        type:String,
+        required:true,
+    },
+    BillingAddress:{
+        type:String,
+        required:true,
+    },
+    BillingCity:{
+        type:String,
+        required:true,
+    },
+    BillingState:{
+        type:String,
+        required:true,
+    },
+    BillingZip:{
+        type:String,
+        required:true
+    },
+    BillingCountry:{
+        type:String,
+        required:true,
+    },
+    ShippingFullName:{
+        type:String,
+        required:true,
+    },
+    ShippingAddress:{
+        type:String,
+        required:true,
+    },
+    ShippingCity:{
+        type:String,
+        required:true,
+    },
+    ShippingState:{
+        type:String,
+        required:true,
+    },
+    ShippingZip:{
+        type:String,
+        required:true,
+    },
+    ShippingCountry:{
+        type:String,
+        required:true,
+    },
+    CompletedAt:{
+        type:String,
+        required:true,
+    },
+    AdditionalComments:{
+        type:String,
+        required:true,
+    },
+    
+},{
+    timestamps:true
+});
+const orderItem = mongoose.Schema({
+    OrderId:{
+        type:String,
+        required:true,
+    },
+    ProductTypeId:{
+        type:String,
+        required:true,
+    },
+    MeasurmentAttributeValues:{
         type:Object,
         required:true,
     },
-   
-    
-    
-
+    ItemCost:{
+        type:Number,
+        required:true,
+    },
+    ItemDiscount:{
+        type:Number,
+        required:true,
+    },
+    ItemSalesTax:{
+        type:Number,
+        required:true,
+    },
+    DeliveryPickupStatus:{
+        type:Number,
+        required:true,
+    },
+    ItemStatus:{
+        type:String,
+        required:true,
+    },
+    TrialDate:{
+        type:Date,
+        required:true,
+    },
+   CompletedAt:{
+    type:String,
+    required:true,
+   },
+   AdditionalComments:{
+    type:String,
+    required:true,
+   },
+   ExpectedCompletionDate:{
+    type:Date,
+    required:true,
+   },
+},{
+    timestamps:true
 });
-
-const measurment = mongoose.Schema(
-
+const orderPayment = mongoose.Schema(
     {
-        Order_id:{
+        OrderId:{
             type:String,
             required:true,
         },
-        Mesurment:{
+        PaymentAmount:{
             type:Object,
             required:true,
+        },
+        AdditionalComments:{
+            type:String,
+            required:true,
+        },
+        PaymentDate:{
+            type:Date,
+            required:true,
         }
+    },
+    {
+        timestamps:true,
     }
-) 
-
+); 
 const Client = mongoose.model("Clients",client);
-const Stores = mongoose.model("Store",stores);
-const Customer = mongoose.model("Customer",customer);
+const Users = mongoose.model("User",users);
+const Role = mongoose.model("Role",role);
+const UserInRole = mongoose.model("UserInRole",userInRole);
+const ClientShops = mongoose.Schema("ClientShop",clientShops)
+const Customer = mongoose.model("Customer",customers);
+const ProductType = mongoose.model("ProductType",productType);
 const Order = mongoose.model("Order",order);
-const Cloth = mongoose.model("Cloth",cloth);
-const Measurment = mongoose.model("Measurment",measurment);
+const OrderItem = mongoose.model("Cloth",orderItem);
+const OrderPayment = mongoose.model("Measurment",orderPayment);
 module.exports = {
     Client,
-    Stores,
+    Users,
+    Role,
+    UserInRole,
+    ClientShops,
     Customer,
+    ProductType,
     Order,
-    Cloth,
-    Measurment
-}
+    OrderItem,
+    OrderPayment,
+};
